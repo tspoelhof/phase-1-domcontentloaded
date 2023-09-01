af475d5fdd8b452e2dd59ffa563ac9a730629a09
Thomas Spoelhof <thomasjspoelhof@gmail.com>
2023-08-31 21:59:40 -0400

    DomContent

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    const domElement = document.querySelector('p');
+    domElement.textContent = "This is really cool!";
+});
